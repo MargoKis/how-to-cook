3324fbba89a326195522268bda926b1d57037b12
Marharyta <margaritakislajalemon@gmail.com>
2023-11-22 22:27:38 +0200

    changed design

--- script.js
+++ script.js
@@ -12,18 +12,12 @@ getMeal.addEventListener("click", () => {
 function createMeal(meal) {
   let ingredientsHTML = "";
   let additionalIngredientsHTML = "";
-  for (let i = 1; i <= 13; i++) {
+  for (let i = 1; i <= 20; i++) {
     const ingredient = meal[`strIngredient${i}`];
     if (ingredient) {
-      if (i <= 5) {
-        ingredientsHTML += `<p class="recipe__food-item">${ingredient}</p>`;
-      } else {
-        additionalIngredientsHTML += `<p class="recipe__food-item">${ingredient}</p>`;
-      }
+      ingredientsHTML += `<p class="recipe__food-item">${ingredient}</p>`;
     }
   }
-
-  const showMoreButton = `<button class="showBtn">more ingredients</button>`;
 
   mealContainer.innerHTML = `
     <div class="recipe__in-block">
@@ -34,31 +28,15 @@ function createMeal(meal) {
             <p class="recipe__country">${meal.strArea}</p>
             <p class="recipe__category">${meal.strCategory}</p>
           </div>
-          <div class="recipe__all-ingredients">
           <p class="recipe__text-ingredients">ingredients</p>
-            <div class="recipe__first-ingredients">${ingredientsHTML}</div>
-            <div class="textMore" style="display: none;">${additionalIngredientsHTML}</div>
-            ${ingredientsHTML.length > 5 ? showMoreButton : ""}
-          </div>
         </div>
+        <div class="recipe__all-ingredients">
+            <div class="recipe__first-ingredients">${ingredientsHTML}</div>
+            <div class="textMore">${additionalIngredientsHTML}</div>
+          </div>
         <div class="content">
           <p class="how-to-cook"><b>How to cook:</b> ${meal.strInstructions}</p>
         </div>
     </div>
     `;
-
-  if (ingredientsHTML.length > 5) {
-    const ingredientsContainer = mealContainer.querySelector(".ingredients");
-    const textMoreContainer = mealContainer.querySelector(".textMore");
-    const showMoreBtn = mealContainer.querySelector(".showBtn");
-
-    showMoreBtn.addEventListener("click", () => {
-      textMoreContainer.style.display =
-        textMoreContainer.style.display === "none" ? "block" : "none";
-      showMoreBtn.innerText =
-        textMoreContainer.style.display === "none"
-          ? "more ingredients"
-          : "less ingredients";
-    });
-  }
 }
